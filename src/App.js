f0d60e42bf90e5d968b9d2cad0ef3a3e88880020
Michael Mass <michaelmass@Michaels-MacBook-Pro.local>
2023-12-14 21:46:32 -0600

    Capstone

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
+import MassMovies from './pages/MassMovies';
+import MoviePage from './pages/MoviePage';
+import TVShow from './pages/TVShow';
+import Player from './pages/Player';
+// import BackgroundImage from './components/BackgroundImage';
+import Header from './components/Header';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+        <Routes>
+          <Route exact path='/login' element={<LoginPage/>}/>
+          <Route exact path='/signup' element={<SignUpPage/>}/>
+          <Route exact path='/Player' element={<Player/>}/>
+          <Route exact path='/tv' element={<TVShow/>}/>
+          <Route exact path='/' element={<MassMovies/>}/>
+          <Route exact path='/movie' element={<MoviePage/>}/>
+        </Routes>
+        <Header/>
+     
+    </BrowserRouter>
+    
   );
 }
 
---
+++ src/utils/firebase-config.js
@@ -0,0 +1,15 @@
+
+import { initializeApp } from "firebase/app";
+import {getAuth} from 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBKkrHAKTiPIQ0jCQcUnrb80Agr6ul0qIA",
+  authDomain: "massmovies-44c8d.firebaseapp.com",
+  projectId: "massmovies-44c8d",
+  storageBucket: "massmovies-44c8d.appspot.com",
+  messagingSenderId: "660715862090",
+  appId: "1:660715862090:web:793e2d7278b04f0086ceb3"
+};
+
+const app = initializeApp(firebaseConfig);
+export const firebaseAuth = getAuth(app)
